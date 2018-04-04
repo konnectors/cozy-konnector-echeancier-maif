b6d8a73d526e030f602acc86c5a1e7648d020b97
doubleface <christophe@cozycloud.cc>
2018-04-04 17:15:46 +0200

    feat: konnector checklist

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@
 // In the future, will be set by the stack
 process.env.SENTRY_DSN =
   process.env.SENTRY_DSN ||
-  "https://1bdc9628c1724cb899ce99bb547efd19:6bd1ecd2e64e42558499c9b2a5d1a0e7@sentry.cozycloud.cc/17";
+  'https://1bdc9628c1724cb899ce99bb547efd19:6bd1ecd2e64e42558499c9b2a5d1a0e7@sentry.cozycloud.cc/17'
 
-const pdfBillsHelper = require("./pdfBillsHelper.js");
+const pdfBillsHelper = require('./pdfBillsHelper.js')
 
 const {
   BaseKonnector,
   saveBills,
   requestFactory,
   log
-} = require("cozy-konnector-libs");
+} = require('cozy-konnector-libs')
 
 let request = requestFactory({
   cheerio: true,
   json: false,
   //debug: true,
   jar: true
-});
+})
 
-const connector = new BaseKonnector(start);
+const connector = new BaseKonnector(start)
 
 function start(fields) {
   return connector
@@ -29,62 +29,62 @@ function start(fields) {
     .then(connector.getInfos)
     .then(connector.pdfToJson)
     .then(connector.extractBills)
-    .then(data => connector.saveBills(data, fields));
+    .then(data => connector.saveBills(data, fields))
 }
 
 connector.initSession = function(fields) {
-  log("info", "Init session");
-  const baseUrl = "https://connect.maif.fr";
+  log('info', 'Init session')
+  const baseUrl = 'https://connect.maif.fr'
   return request({
-    url: baseUrl + "/connect/s/popup/identification",
-    method: "GET",
+    url: baseUrl + '/connect/s/popup/identification',
+    method: 'GET',
     resolveWithFullResponse: true
   }).then(response => {
-    const $ = response.body;
+    const $ = response.body
 
     let form = {
       j_username: fields.login,
       j_password: fields.password
-    };
-
-    const connectUrl =
-      baseUrl + $("form[id='j_spring_security_check']").attr("action");
-    const inputs = Array.from(
-      $("form[id='j_spring_security_check']").find("input[type='hidden']")
-    );
-    for (var i in inputs) {
-      const input = $(inputs[i]);
-      form[input.attr("name")] = input.attr("value");
     }
 
-    return { connectUrl, form };
-  });
-};
+    const connectUrl =
+      baseUrl + $("form[id='j_spring_security_check']").attr('action')
+    const inputs = Array.from(
+      $("form[id='j_spring_security_check']").find("input[type='hidden']")
+    )
+    for (var i in inputs) {
+      const input = $(inputs[i])
+      form[input.attr('name')] = input.attr('value')
+    }
+
+    return { connectUrl, form }
+  })
+}
 
 connector.logIn = function(connectData) {
-  log("info", "Logging in");
-  log("secret", "Data : " + JSON.stringify(connectData));
+  log('info', 'Logging in')
+  log('secret', 'Data : ' + JSON.stringify(connectData))
 
   return request({
     url: connectData.connectUrl,
-    method: "POST",
+    method: 'POST',
     form: connectData.form,
     resolveWithFullResponse: true
   }).then(response => {
     // Check connect ok
-    log("debug", "Logging status code : " + response.statusCode);
-    return;
-  });
-};
+    log('debug', 'Logging status code : ' + response.statusCode)
+    return
+  })
+}
 
 connector.getInfos = function() {
   return request({
-    url: "https://www.maif.fr/informationspersonnelles/accueilInfoPerso.action",
+    url: 'https://www.maif.fr/informationspersonnelles/accueilInfoPerso.action',
     resolveWithFullResponse: true
   }).then(response => {
     const accessToken = response.request.uri.hash.match(
       /#token=(.*)&refreshToken=/
-    )[1];
+    )[1]
 
     request = requestFactory({
       cheerio: false,
@@ -92,47 +92,47 @@ connector.getInfos = function() {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
-    });
+    })
     return Promise.all([
-      request("https://espacepersonnel.maif.fr/societaire/api/societaire/me"),
+      request('https://espacepersonnel.maif.fr/societaire/api/societaire/me'),
       accessToken
-    ]);
-  });
-};
+    ])
+  })
+}
 
 connector.pdfToJson = function([infos, accessToken]) {
   const pdfUrl = `https://espacepersonnel.maif.fr${
     infos.avisEcheance.link
-  }&token=${accessToken}`;
+  }&token=${accessToken}`
   return request({ url: pdfUrl, encoding: null }).then(data => {
-    return { pdfUrl, data, infos };
-  });
-};
+    return { pdfUrl, data, infos }
+  })
+}
 
 connector.extractBills = function({ pdfUrl, data, infos }) {
   return pdfBillsHelper.getBills(new Uint8Array(data)).then(extractedData => {
-    return { pdfUrl, infos, extractedData };
-  });
-};
+    return { pdfUrl, infos, extractedData }
+  })
+}
 
 connector.saveBills = function({ pdfUrl, infos, extractedData }, fields) {
-  log("debug", "Creating Bills with !" + pdfUrl);
-  const bills = [];
+  log('debug', 'Creating Bills with !' + pdfUrl)
+  const bills = []
 
   for (var idx in extractedData) {
     bills.push({
       amount: extractedData[idx].amount,
       date: extractedData[idx].date.toDate(),
       fileurl: pdfUrl,
-      filename: "Avis_echeance.pdf",
-      vendor: "maif",
+      filename: 'Avis_echeance.pdf',
+      vendor: 'maif',
       maifdateadhesion: infos.dateAdhesion,
       maiftelephone: extractedData[idx].maiftelephone,
       maifnumsocietaire: infos.numeroSocietaireFormate
-    });
+    })
   }
 
-  return saveBills(bills, fields, { identifiers: ["MAIF"] });
-};
+  return saveBills(bills, fields, { identifiers: ['MAIF'] })
+}
 
-module.exports = connector;
+module.exports = connector
--- src/pdfBillsHelper.js
+++ src/pdfBillsHelper.js
@@ -1,5 +1,5 @@
-const moment = require("moment");
-const pdfjs = require("pdfjs-dist");
+const moment = require('moment')
+const pdfjs = require('pdfjs-dist')
 
 exports.getBills = function(pdfUrl) {
   return pdfjs
@@ -7,28 +7,28 @@ exports.getBills = function(pdfUrl) {
     .then(doc => doc.getPage(1))
     .then(page => page.getTextContent())
     .then(content => {
-      const result = cleanItems(content.items);
-      const maiftelephone = getDataAfterPrefix(result, "Téléphone : ");
-      const amounts = getAmounts(result);
+      const result = cleanItems(content.items)
+      const maiftelephone = getDataAfterPrefix(result, 'Téléphone : ')
+      const amounts = getAmounts(result)
       return getDates(result).map((dateStr, index) => {
-        const date = moment(dateStr, "D MMMM YYYY", "fr");
-        const amount = parseFloat(amounts[index]);
+        const date = moment(dateStr, 'D MMMM YYYY', 'fr')
+        const amount = parseFloat(amounts[index])
 
         return {
           maiftelephone,
           date,
           amount
-        };
-      });
-    });
-};
+        }
+      })
+    })
+}
 
 function getDataAfterPrefix(items, prefix) {
   return items
     .filter(item => item.content.startsWith(prefix))
     .map(filteredItem => {
-      return filteredItem.content.substring(prefix.length);
-    })[0];
+      return filteredItem.content.substring(prefix.length)
+    })[0]
 }
 
 function cleanItems(items) {
@@ -36,40 +36,40 @@ function cleanItems(items) {
     content: item.str,
     height: item.height,
     top: item.transform[5]
-  }));
+  }))
 }
 
 function getAmounts(items) {
-  return selectTextsByHeader(items, "Montant en euros");
+  return selectTextsByHeader(items, 'Montant en euros')
 }
 
 function getDates(items) {
-  return selectTextsByHeader(items, "Date de prélèvement").reduce(
+  return selectTextsByHeader(items, 'Date de prélèvement').reduce(
     (memo, item) => {
       if (Number(item)) {
-        memo[memo.length - 1] += ` ${item}`;
+        memo[memo.length - 1] += ` ${item}`
       } else {
-        memo.push(item);
+        memo.push(item)
       }
-      return memo;
+      return memo
     },
     []
-  );
+  )
 }
 
 function selectTextsByHeader(items, header) {
-  const firstRow = items.filter(item => item.content === header);
+  const firstRow = items.filter(item => item.content === header)
   const result = firstRow.map(firstCell => {
     const selectedCells = filterCellsByTop(
       items,
       firstCell.top + firstCell.height + 10,
       firstCell.top - 10
-    );
-    return selectedCells.slice(1).map(item => item.content);
-  });
-  return result[0].concat(result[1]);
+    )
+    return selectedCells.slice(1).map(item => item.content)
+  })
+  return result[0].concat(result[1])
 }
 
 function filterCellsByTop(items, top, bottom) {
-  return items.filter(item => item.top <= top && item.top >= bottom);
+  return items.filter(item => item.top <= top && item.top >= bottom)
 }
